f69e4e5c1036c183d953c42d50e72093de484a77
Eduardo <dudueduardosantos2018@gmail.com>
2026-06-14 19:06:21 -0300

    Adiciona conteúdo ao post do blog

--- src/app/core/data/blog-posts.data.ts
+++ src/app/core/data/blog-posts.data.ts
@@ -16,6 +16,19 @@ export const BLOG_POSTS_DATA: BlogPost[] = [
   },
   {
     "id": 2,
+    "title": "Fundamentos AWS Cloud + IA | IAM, EC2, S3, RDS, VPC, CloudWatch, Linux, Python, Segurança e Automação",
+    "excerpt": "Formação em computação em nuvem com foco em fundamentos AWS, infraestrutura global,computação, armazenamento, redes, segurança, banco de dados, Linux, Python, automação, governança em nuvem e introdução à Inteligência Artificial.",
+    "date": "2024-07-30",
+    "tags": [
+      "AWS",
+      "Cloud Computing",
+      "IA"
+    ],
+    "image": "assets/images/curso_aws_escola_da_nuvem.jpg",
+    "url": "https://www.linkedin.com/posts/carlos-eduardo-tecnologia_forms-csharp-progamaaexaeto-activity-7196310312474574848-K9Fq?utm_source=social_share_send&utm_medium=member_desktop_web&rcm=ACoAADtzvM8B1xWBqruxDwP84CqRK8B8LSu68TY"
+  },
+  {
+    "id": 3,
     "title": "Leitura do Livro Clear Code",
     "excerpt": "A leitura do livro de Clean Code foi uma jornada de aprendizado profundo que me ajudou a melhorar minhas habilidades de codificação e aprimorar minha abordagem de desenvolvimento de software.",
     "date": "2024-08-01",
@@ -28,7 +41,7 @@ export const BLOG_POSTS_DATA: BlogPost[] = [
     "url": "https://www.linkedin.com/posts/carlos-eduardo-tecnologia_progamaaexaeto-csharp-javascript-activity-7211875555321774080-6Qwz?utm_source=share&utm_medium=member_desktop&rcm=ACoAADtzvM8B1xWBqruxDwP84CqRK8B8LSu68TY"
   },
   {
-    "id": 3,
+    "id": 4,
     "title": "Cursos para aprimorar suas habilidades em Angular, Laravel e GIT",
     "excerpt": "Concluçãos dos cursos de Angular e GIT e andamento do curso de laravel para aprimorar minhas habilidades em desenvolvimento web.",
     "date": "2025-08",
@@ -41,7 +54,7 @@ export const BLOG_POSTS_DATA: BlogPost[] = [
     "url": "https://www.linkedin.com/in/carlos-eduardo-tecnologia/details/certifications/"
   },
   {
-    "id": 4,
+    "id": 5,
     "title": "Evento da Emaster sobre IA Generativa com AWS",
     "excerpt": "Tive a oportunidade de participar de um evento organizado pela eMaster , com foco em IA generativa, especificamente utilizando as soluções da Amazon Web Services (AWS) . ",
     "date": "2024-09",
@@ -54,8 +67,8 @@ export const BLOG_POSTS_DATA: BlogPost[] = [
     "url": "https://www.linkedin.com/posts/carlos-eduardo-tecnologia_ia-inteligenciaartificial-iagenerativa-activity-7239652107249709056-ygF-?utm_source=share&utm_medium=member_desktop&rcm=ACoAADtzvM8B1xWBqruxDwP84CqRK8B8LSu68TY"
   },
   {
-    "id": 5,
-    "title": "Seminários  ISO 37001",
+    "id": 6,
+    "title": "Evento  ISO 37001",
     "excerpt": "Seminario dado pela Produs sobre Antisuborno e Anticorrupção, com foco em ISO 37001.",
     "date": "2025-09-20",
     "tags": [
